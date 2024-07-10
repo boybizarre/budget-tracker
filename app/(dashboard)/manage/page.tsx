f050b42e9dbff1f5ce1309a18fac672d2633276a
boybizarre <106738064+boybizarre@users.noreply.github.com>
2024-07-10 01:16:19 +0100

    feat: working on manage page

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -1,2 +1,21 @@
 'use client';
 
+import React from 'react';
+
+function page() {
+  return (
+    <>
+      {/* header */}
+      <div className='border-b bg-card'>
+        <div className='container flex flex-wrap items-centre justify-between gap-6 py-6'>
+          <div>
+            <h2 className='text-3xl font-bold'>Manage</h2>
+            <p className='text-muted'>
+              Manage your account settings and categories
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
